51f406fd7715ddc5d1119d61b9023fbd29e19f24
P_7_Rathore <rathore@example.com>
2020-04-08 14:27:34 +0530

    diff b/w single and double code string(Numbers and Strings)

---
+++ flicks.rb
@@ -0,0 +1,26 @@
+# data print with single code 
+
+puts 'Mikey loves Dhoom'
+
+puts 'Mikey\'s favorite movie is Dhoom'
+
+movie = 'Dhoom'
+
+puts 'Mikey\'s favorite movie is ' + movie
+
+rank = 10
+
+puts movie +' has a rank of ' + rank.to_s
+
+# data print with duoble code
+
+puts "Mikey love Dhoom"
+
+puts "Mikey's favorite movie is Dhoom"
+
+movie = "Dhoom"
+puts "Mikey's favorite movie is #{movie}" 
+
+rank = 10
+puts "#{movie} has a rank of #{rank}"
+puts "#{movie} has a rank of #{rank * 2}"
