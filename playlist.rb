3134e252cc3125e6efd38a3074446b8af3fc533f
P_7_Rathore <rathore@example.com>
2020-04-09 16:55:23 +0530

    create_playlist_specs_file(Conditional And TDD part-2)

--- playlist.rb
+++ playlist.rb
@@ -10,18 +10,41 @@ class Playlist
 		@movies << movie
 	end
 
+	def roll_die
+		rand(1..6)
+	end
+	
 	def play
+		puts "****************#{@name} Playlist**************" 
 		puts @movies
 
 		@movies.each do |movie|
-			movie.thumbs_up
-			puts movie
+			number_rolled = roll_die
+			# if number_rolled < 3
+			# 	movie.thumbs_down
+			# 	puts "#{movie.title} got a thumbs down."
+			# elsif number_rolled < 5
+			# 	puts "#{movie.title} was skipped."
+			# else
+			# 	movie.thumbs_up
+			# 	puts "#{movie.title} got a thumbs up"
+			# end	
+
+			case number_rolled
+			when 1..2
+				movie.thumbs_down
+				puts "#{movie.title} got a thumbs down."
+			when 3..4
+				puts "#{movie.title} was skipped."
+			else
+				movie.thumbs_up
+				puts "#{movie.title} got a thumbs up"
+			end	
 		end
 	end
 
 end
 puts "=================playlist=============="
-puts "****************Piyush Playlist**************"
 playlist1 = Playlist.new("Piyush")
 movie1 = Movie.new("Dhoom",10) 
 
@@ -34,7 +57,6 @@ movie3 = Movie.new("krish",7)
 playlist1.add_movie(movie3)
 playlist1.play
 
-puts "****************Deepak Playlist**************"
 playlist2 = Playlist.new("Deepak")
 
 playlist2.add_movie(movie1)
---
+++ playlist_spec.rb
@@ -0,0 +1,47 @@
+require_relative "playlist"
+
+describe Playlist do 
+	before do
+		@playlist = Playlist.new("Piyush")
+	end
+
+	context "being played with one movie" do
+		before do
+			@initial_rank = 10
+			@movie = Movie.new("Dhoom", @initial_rank)
+			@playlist.add_movie(@movie)
+		end
+
+		it "gives the movie a thumbs up if a high number is rolled" do
+			# @playlist.play(5)
+			# @movie.rank.should == @initial_rank +1
+			
+			@playlist.stub(:roll_die).and_return(5)
+
+			@playlist.play
+			@movie.rank.should == @initial_rank +1
+		end
+
+		it "skips the movie if a medium number is rolled" do
+			# @playlist.play(3)
+			# @movie.rank.should == @initial_rank
+			
+			@playlist.stub(:roll_die).and_return(3)
+			
+			@playlist.play
+			@movie.rank.should == @initial_rank
+		end
+
+		it "give the movie a thumbs down if a low number is rolled" do
+			# @playlist.play(1)
+			# @movie.rank.should == @initial_rank - 1
+
+			@playlist.stub(:roll_die).and_return(1)
+
+			@playlist.play
+			@movie.rank.should == @initial_rank - 1
+		end
+
+	end
+
+end
