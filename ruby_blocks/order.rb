51d9274cc1d74cc8014f957cb29ad96a1193ed0e
P_7_Rathore <rathore@example.com>
2020-04-13 20:57:45 +0530

    use_of_select_detect_any?(ruby_block-4-Enumerable)

--- ruby_blocks/order.rb
+++ ruby_blocks/order.rb
@@ -1,38 +1,50 @@
 class Order
-	attr_reader :email, :total
-	
-	def initialize(email, total)
+
+	TAX_TABLE = {"CO" => 0.02, "MT" => 0.00, "AZ" => 0.04}
+	attr_reader :email, :total, :state, :status
+
+	def initialize(email, state, total, status=:pending)
 		@email = email
-		@total= total
+		@state = state
+		@total =total
+		@status =status
+	end
+
+	def tax
+		total * TAX_TABLE[state]
 	end
 
 	def to_s
-		"#{email}: $#{total}"
+		"#{email} (#{state}): $#{total} - #{status}"
 	end
 end
 
 orders = []
+orders << Order.new("customer1@example.com", "MT", 300)
+orders << Order.new("customer2@example.com", "AZ", 400, :completed)
+orders << Order.new("customer3@example.com", "CO", 200)
+orders << Order.new("customer4@example.com", "CO", 100, :completed)
 
-1.upto(5) do |n|
-	orders << Order.new("customer#{n}@example.com", n * 10)
+puts orders
+
+puts "big_orders:"
+big_orders = orders.select{ |o| o.total >= 300}
+puts big_orders # its return arry with values
+
+big_orders = orders.select { |o| puts o.total >=300  } # its print true, true, false, false
+puts big_orders # its return nil because puts return nil
+
+puts orders.select {|o| o.total >=300 } #its return array with values
+
+puts orders.select do |o|
+	o.total >= 300 # its return <Enumerable :90909090> because in this select is not bounded and with single line select is bounded 
 end
 
-puts "News Letter Emails: "
+puts "Small orders:"
+small_orders = orders.reject {|o| o.total >= 300}
+puts small_orders
 
-# orders.each do |order|
-# 	puts order.email
-# end
+puts orders.any? {|o| o.status == :pending } #if one get true return true after that not check other remaning values  
 
-orders.each { |order| puts order.email }
-
-sum = 0
-
-orders.each {|order| sum += order.total}
-puts "Total sales: $#{sum}"
-
-taxes = {"CO" => 0.02, "MT" => 0.00, "AZ" => 0.04}
-
-taxes.each do |key, value|
-	puts "#{key}: #{value * 100} %"
-end
-
+order = orders.detect {|o| o.status == :pending} # if conition true then its return data not true(like any?) and its also not check other remaning values
+puts order
